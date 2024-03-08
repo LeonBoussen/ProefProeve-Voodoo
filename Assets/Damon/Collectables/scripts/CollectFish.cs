09087f3030c94971b086fc86932c5503d9ce0b8a
Damon-Jay <damon.jellema2004@gmail.com>
2024-03-08 14:33:17 +0100

    leaderboard

--- Assets/Damon/Collectables/scripts/CollectFish.cs
+++ Assets/Damon/Collectables/scripts/CollectFish.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class CollectFish : MonoBehaviour
 {
+    [SerializeField] private float speed;
+
     public GameManager GM;
+    public Vector3 rotationAngles = new Vector3(-90, 90, 90);
+
+    private void FishMovement()
+    {
+        /*
+        Add the accelaration to the speed value and use Clamp to make sure the speed value stays between 0 and maxSpeed value.
+        Then use that speed value to update chuck position on the Y axis
+        */
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+    }
+
     private void OnTriggerEnter2D(Collider2D Hook)
     {
        if(Hook.name == "Player")
@@ -12,5 +27,11 @@ public class CollectFish : MonoBehaviour
             GM.CollectedFish();
             Destroy(gameObject);
         } 
+    }
+
+    void Update()
+    {
+        // Rotate the GameObject by the specified angles
+        transform.Rotate(rotationAngles);
     }
 }
