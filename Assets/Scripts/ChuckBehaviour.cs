4ba1c2037025315574c046e3c84f1054554d2a39
Damon-Jay <damon.jellema2004@gmail.com>
2024-03-11 15:11:56 +0100

    leon en timo scripts

--- Assets/Scripts/ChuckBehaviour.cs
+++ Assets/Scripts/ChuckBehaviour.cs
@@ -8,13 +8,8 @@ using UnityEngine;
 
 public class ChuckBehaviour : MonoBehaviour
 {
+    
     [SerializeField] private float speed; //Current falling speed of chunk
-    [SerializeField] private float maxSpeed; //Maximun speed a chuck should move
-    [SerializeField] private float accelaration; //How fast should the chunk speed speedup unitl maximun speed.
-    [SerializeField] private GameObject self; // refrence to own Chuck
-    [SerializeField] private GameObject pivot; // refrence to custom pivot point for self
-    [SerializeField] private GameObject newChuck; // newchuck to spawn
-    [SerializeField] private GameObject spawnLocation; // GameObject in the game scene to get the transform from for spawning new chucks
 
 
     private void ChunkMovement()
@@ -24,7 +19,7 @@ public class ChuckBehaviour : MonoBehaviour
         Then use that speed value to update chuck position on the Y axis
         */
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
+        
     }
 
     void Update()
